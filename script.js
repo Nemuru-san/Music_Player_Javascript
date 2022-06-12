71a76829780972c629f4d427c079c371f2dc09b0
Nemuru-san <nimemanta@gmail.com>
2022-06-12 15:51:51 +0700

    selesai

--- script.js
+++ script.js
@@ -55,13 +55,28 @@ function prevSong() {
 function nextSong() {
     songIndex++;
 
-    if (songIndex < songCollection.length - 1) {
+    if (songIndex > songCollection.length - 1) {
         songIndex = 0;
     }
 
     loadSong(songCollection[songIndex]);
 
     playSong();
+}
+
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercentage = (currentTime / duration) * 100;
+
+    progress.style.width = `${progressPercentage}%`;
+}
+
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
 }
 
 // event listener 
@@ -78,3 +93,9 @@ btnPlay.addEventListener('click', () => {
 // change song
 btnPrev.addEventListener('click', prevSong);
 btnNext.addEventListener('click', nextSong);
+
+audio.addEventListener('timeupdate', updateProgress);
+
+containerProgress.addEventListener('click', setProgress);
+
+audio.addEventListener('ended', nextSong);
