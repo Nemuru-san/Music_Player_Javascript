5752e3b7851732afa0f5ff50635142ffc175f1e8
Nemuru-san <nimemanta@gmail.com>
2022-06-12 15:20:46 +0700

    masalah pada favicon selesai

---
+++ script.js
@@ -0,0 +1,80 @@
+const containerMusic = document.querySelector('.musicContainer');
+const btnPrev = document.querySelector('#prev');
+const btnPlay = document.querySelector('#play');
+const btnNext = document.querySelector('#next');
+
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const containerProgress = document.querySelector('.progressContainer');
+const title = document.querySelector('#titleMusic');
+const cover = document.querySelector('#cover');
+
+// song titleMusic
+const songCollection = ['feint - word', 'GEazy - Get Back Up', 'no sugar'];
+
+// song trackCollection
+let songIndex = 2;
+
+// load song with DOM element
+loadSong(songCollection[songIndex]);
+
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `image/${song}.jpg`;
+}
+
+function playSong() {
+    containerMusic.classList.add('play');
+    btnPlay.querySelector('.fa-solid').classList.remove('fa-play');
+    btnPlay.querySelector('.fa-solid').classList.add('fa-pause');
+
+    audio.play();
+}
+
+function pauseSong() {
+    containerMusic.classList.remove('play');
+    btnPlay.querySelector('.fa-solid').classList.add('fa-play');
+    btnPlay.querySelector('.fa-solid').classList.remove('fa-pause');
+
+    audio.pause();
+}
+
+function prevSong() {
+    songIndex--;
+
+    if (songIndex < 0) {
+        songIndex = songCollection.length - 1;
+    }
+
+    loadSong(songCollection[songIndex]);
+
+    playSong();
+}
+
+function nextSong() {
+    songIndex++;
+
+    if (songIndex < songCollection.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songCollection[songIndex]);
+
+    playSong();
+}
+
+// event listener 
+btnPlay.addEventListener('click', () => {
+    const isPlaying = containerMusic.classList.contains('play');
+
+    if (isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
+
+// change song
+btnPrev.addEventListener('click', prevSong);
+btnNext.addEventListener('click', nextSong);
